b7410ec20f739a2034594ae318ebee205a03a43d
Civitasv <2309780282@qq.com>
2021-08-29 17:54:22 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,6 @@
+# POIKit (Python Version)
+
+## Technology
+
+- http: [requests module](https://docs.python-requests.org/en/master/)
+- gui: [Kivy](https://kivy.org/#home)
---
+++ tests/test_sample.py
@@ -0,0 +1,11 @@
+from context import poikit
+import unittest
+
+class AdvancedTestSuite(unittest.TestCase):
+    """Advanced test cases."""
+
+    def test_thoughts(self):
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
