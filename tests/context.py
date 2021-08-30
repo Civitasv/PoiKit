c96630559ed624c48088b2d7400e668483914529
Civitasv <2309780282@qq.com>
2021-08-30 17:43:59 +0800

    代码结构小修

--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import poikit
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import poikit
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,11 +5,13 @@
 from .context import poikit
 import unittest
 
+
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
         assert True
+
 
 if __name__ == '__main__':
     unittest.main()
